9ac859cfa5fbdba2a1f522f3f68a2f4a98302d06
Kevv-commits <knganga28@gmail.com>
2021-06-17 12:51:34 -0700

    puts output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main ()
+/*
+ *main function prints string
+ *Retuns 0
+*/
+int main (void)
 {
   puts("Programming is like building a multilingual puzzle\n");
     return (0);
