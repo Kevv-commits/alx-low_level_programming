afa5ce8c30f0f7d0dd0d4ad5d46c25bff0d33985
Kevv-commits <knganga28@gmail.com>
2021-06-17 13:50:11 -0700

    final test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,"
+ *main - Entry point
  *
- * Return: 1.
+ *Return: Always 0 (success)
  */
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
-
+	fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
 	return (1);
-
-
 }
