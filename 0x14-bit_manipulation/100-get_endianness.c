93bf659bf67b8616eb46619cd3d68016b7f64e46
Kevv-commits <knganga28@gmail.com>
2021-08-05 12:30:39 -0700

    fixed

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,10 +7,11 @@
 
 int get_endianness(void)
 {
-	int x;
-	int little_or_big;
+	int num = 1;
+	char *endian = (char *)&num;
 
-	x = 1;
-	little_or_big = (int) (((char *)&x)[0]);
-	return (little_or_big);
+	if (*endian == 1)
+		return (1);
+
+	return (0);
 }
