aed38e2e1fcc036fc9b9b57f6d6ad7daf6e0c9f2
Kevv-commits <knganga28@gmail.com>
2021-08-09 12:10:18 -0700

    creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,31 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * append_text_to_file - appends text at the end of a file.
- * @filename: name of the file to append the text.
- * @text_content: the NULL terminated string to add at the end of the file.
- * Return: 1 on success, -1 on failure
+ * create_file - function to create a file
+ * @filename: file
+ * @text_content: number of letters of the file
+ * Return: numbers of letters or zero it fails
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, length = 0;
+	int fd = 0;
+	int size = 0;
+	int  writ;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	file_descriptor = open(filename, O_RDWR | O_APPEND);
-	if (file_descriptor < 0)
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
-	if (!text_content)
-	{
-		close(file_descriptor);
+	if (text_content == NULL)
 		return (1);
-	}
-
-	while (text_content[length])
-		length++;
-	if (write(file_descriptor, text_content, length) < 0)
+	while (text_content[size])
 	{
-		close(file_descriptor);
-		return (-1);
+		size++;
 	}
-	close(file_descriptor);
+	writ = write(fd, text_content, size);
+	if (writ == -1)
+		return (-1);
+
+	close(fd);
 	return (1);
 }
