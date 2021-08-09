b70261c97f86043631582c4f37b51b5c6c6b4c18
Kevv-commits <knganga28@gmail.com>
2021-08-09 12:17:21 -0700

    creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * create_file - function to create a file
- * @filename: file
- * @text_content: number of letters of the file
- * Return: numbers of letters or zero it fails
+ * create_file - creates a file.
+ * @filename: name of the file to be created.
+ * @text_content: null terminated string to be written to the file.
+ *
+ * Return: 1 on success, -1 on failure.
  */
 
 int create_file(const char *filename, char *text_content)
