335a35a7e8ef2c83e15dc473425302c14ee3ecda
Kevv-commits <knganga28@gmail.com>
2021-08-09 12:13:57 -0700

    creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,30 @@
 #include "main.h"
+
 /**
  * create_file - function to create a file
  * @filename: file
  * @text_content: number of letters of the file
  * Return: numbers of letters or zero it fails
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd = 0;
-	int size = 0;
-	int  writ;
+	int fd, wr, len;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
-	while (text_content[size])
+	if (text_content)
 	{
-		size++;
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		wr = write(fd, text_content, len);
+		if (wr == -1)
+			return (-1);
 	}
-	writ = write(fd, text_content, size);
-	if (writ == -1)
-		return (-1);
-
 	close(fd);
+
 	return (1);
 }
